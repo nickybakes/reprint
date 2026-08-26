a8602b13346d8bc4c1f784e93aa44253611f66a2
Nick Baker <nickmbaker1@gmail.com>
2026-08-26 02:57:22 -0700

    got string replacement for subtags

--- RePrint Unity/Assets/Dev/Frontend/Directories/SubTagDirectory.cs
+++ RePrint Unity/Assets/Dev/Frontend/Directories/SubTagDirectory.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SubTag Directory", menuName = "Scriptable Objects/SubTag Directory")]
@@ -16,41 +17,161 @@ public class SubTagDirectory : ScriptableObject
     {
         SubTagResult results = new SubTagResult();
 
+        int currentIndex = 0;
+        while (currentIndex < promptString.Length)
+        {
+            SubTagString nextTag = FindNextTag(promptString, currentIndex);
+            if (nextTag.index == -1)
+            {
+                break;
+            }
 
+            if (nextTag.isASubTag)
+            {
+                int subtagIndex = GetSubTagDirectoryIndex(nextTag.tag);
+                if (subtagIndex != -1)
+                {
+                    SubTagResult replacementResult = GetSubTagResults(SubTags[subtagIndex].Replacement);
+
+                    // for (int i = 0; i < SubTags[subtagIndex].Parameters.Length; i++)
+                    // {
+                    //     string paramaterCode = "%" + SubTags[subtagIndex].Parameters[i];
+                    //     while (replacementResult.promptReplaceString.Contains(paramaterCode))
+                    //     {
+                    //         replacementResult.promptReplaceString = replacementResult.promptReplaceString.Replace(paramaterCode, nextTag.parameters[i]);
+                    //     }
+                    // }
+
+                    promptString = promptString.Remove(nextTag.index, nextTag.length + 1);
+                    promptString = promptString.Insert(nextTag.index, replacementResult.promptReplaceString);
+
+                    currentIndex = nextTag.index + replacementResult.promptReplaceString.Length;
+                    continue;
+                }
+                else
+                {
+                    int colorIndex = GetColorDirectoryIndex(nextTag.tag);
+                    if (colorIndex != -1)
+                    {
+                        string colorString = "<color=#" + Colors[colorIndex].Color.ToHexString() + ">";
+                        promptString = promptString.Remove(nextTag.index, nextTag.length + 1);
+                        promptString = promptString.Insert(nextTag.index, colorString);
+
+                        currentIndex = nextTag.index + colorString.Length;
+                        continue;
+                    }
+                    else
+                    {
+                        int spriteIndex = GetSpriteDirectoryIndex(nextTag.tag);
+                        if (spriteIndex != -1)
+                        {
+                            string spriteString = "<sprite=" + Sprites[spriteIndex].SpriteIndex + ">";
+                            promptString = promptString.Remove(nextTag.index, nextTag.length + 1);
+                            promptString = promptString.Insert(nextTag.index, spriteString);
+
+                            currentIndex = nextTag.index + spriteString.Length;
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            currentIndex = nextTag.index + nextTag.length;
+        }
+
+        results.promptReplaceString = promptString;
         return results;
     }
 
-    private SubTagString FindNextTag(string promptString)
+    private SubTagString FindNextTag(string promptString, int startingIndex)
     {
+        SubTagString subTagString = new SubTagString();
 
-        int openingIndex = promptString.IndexOf('<');
+        int openingIndex = promptString.IndexOf('<', startingIndex);
 
         if (openingIndex != -1)
         {
+            subTagString.index = openingIndex;
+            subTagString.length = 1;
             string subString = promptString.Substring(openingIndex);
             int closingIndex = subString.IndexOf('>');
             if (closingIndex != -1)
             {
+                subTagString.length = closingIndex;
+                string keyword = subString.Substring(1, closingIndex - 1);
 
+                subTagString.tag = keyword;
+
+                int openingParamIndex = keyword.IndexOf('(');
+                if (openingParamIndex != -1)
+                {
+                    string parameterString = keyword.Substring(openingParamIndex);
+                    int closingParamIndex = keyword.IndexOf(')');
+                    if (closingParamIndex != -1)
+                    {
+                        parameterString = parameterString.Substring(1, closingParamIndex - 1);
+                        subTagString.parameters = parameterString.Split(',');
+                        subTagString.tag = keyword.Substring(1, openingParamIndex);
+                    }
+                }
+
+                subTagString.isASubTag = true;
+                return subTagString;
             }
         }
 
-        return null;
+        subTagString.isASubTag = false;
+        return subTagString;
     }
+
+    private int GetSubTagDirectoryIndex(string tag)
+    {
+        for (int i = 0; i < SubTags.Count; i++)
+        {
+            if (SubTags[i].Tag == tag)
+                return i;
+        }
+
+        return -1;
+    }
+
+    private int GetColorDirectoryIndex(string tag)
+    {
+        for (int i = 0; i < Colors.Count; i++)
+        {
+            if (Colors[i].Tag == tag)
+                return i;
+        }
+
+        return -1;
+    }
+
+    private int GetSpriteDirectoryIndex(string tag)
+    {
+        for (int i = 0; i < Sprites.Count; i++)
+        {
+            if (Sprites[i].Tag == tag)
+                return i;
+        }
+
+        return -1;
+    }
+
 }
 
 public class SubTagString
 {
+    public bool isASubTag;
     public string tag;
-    public int index;
+    public int index = -1;
     public int length;
-    public string[] parameters;
+    public string[] parameters = new string[0];
 }
 
 public class SubTagResult
 {
     public string promptReplaceString;
-    public List<string> subDescriptions = new List<string>();
+    public List<SubTagResult> subDescriptions = new List<SubTagResult>();
 }
 
 [Serializable]
---
+++ RePrint Unity/Assets/Dev/Frontend/Directories/SubTagTester.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SubTagTester : MonoBehaviour
+{
+
+    public SubTagDirectory directory;
+
+    public List<string> strings;
+
+    void Start()
+    {
+
+        TestSubTags();
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // EditorApplication.isPlaying need to be set to false to end the game
+        EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+
+
+    public void TestSubTags()
+    {
+        Debug.Log("----------");
+        Debug.Log("Sub Tag Testing Starting");
+
+        for (int i = 0; i < strings.Count; i++)
+        {
+            Debug.Log("String: " + strings[i]);
+            SubTagResult result = directory.GetSubTagResults(strings[i]);
+            Debug.Log(result.promptReplaceString);
+        }
+
+    }
+}
